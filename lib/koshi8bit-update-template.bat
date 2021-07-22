097152f4425edefe4e2f19f9fa373c07bf7c2df2
koshi8bit XXX <koshi8bit@mail.ru>
2021-07-22 14:25:53 +0700

    koshi8bit-update-template.bat

---
+++ lib/koshi8bit-update-template.bat
@@ -0,0 +1,19 @@
+::COPY THIS TO YOUR PROJECT .\lib\
+@echo off
+
+echo SCRIPT DISABLED! DELETE THIS 3 LINES
+pause
+exit
+
+set source="XXX"
+
+set dest=%cd%\kb4\
+:RESTARTT
+rd %dest% /Q /S
+IF EXIST %dest% (
+	echo ACHTUNG! Folder deletion failed!
+    timeout /t 1
+    GOTO RESTARTT
+)
+xcopy %source% %dest% /E /Y
+attrib %dest%* /S +R
